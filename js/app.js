0e8718e57fa231e86630b14cb3ef56b1d61fb495
andrea-isabel <andrea19.93.15@gmail.com>
2017-11-14 17:19:22 -0500

    Proceso text area autoajustable

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 var newTweet = document.querySelector('.new-tweet');
+var totalChar = document.getElementById('total-char');
 
 newTweet.addEventListener('click', expandTweetBox);
+newTweet.addEventListener('keyup', countChar)
+newTweet.addEventListener('keyup', adjustHeight);
 
 function expandTweetBox(event) {
   if(event.target.matches('textarea')) {
+    var tweetBtn = document.getElementById('add-tweet');    
     event.target.className += ' expand-box';
-    var tweetBtn = document.getElementById('submit-tweet');
-    console.log(tweetBtn);
-    tweetBtn.style.display = 'inline';
-    // tweetBtn.textContent = 'Twittear';
-    // document.getElementById('new-tweet-box').appendChild(tweetBtn);
+    tweetBtn.style.display = 'block';
+    totalChar.style.display = 'block';
     console.log(event);
   }
+}
+
+function countChar(event) {
+  var charCounter = newTweet.value.length;
+  totalChar.textContent = 140 - charCounter;
+  console.log(charCounter);
+}
+
+function adjustHeight(event){
+  
+  newTweet.style.height = new (newTweet.scrollHeight - parseFloat(newTweet.style.height)) + 'px';
 }
