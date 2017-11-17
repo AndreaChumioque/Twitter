6eb5011b0c453186e6b217e450aa13b58e7ac2ca
andrea-isabel <andrea19.93.15@gmail.com>
2017-11-16 23:48:22 -0500

    Función clickOut y variar color del contador

--- js/app.js
+++ js/app.js
@@ -6,22 +6,13 @@ var newsfeed = document.getElementById('newsfeed');
 
 var charCounter = 0;
 
-document.addEventListener('click', checkClickOut);
+window.addEventListener('click', checkClickOut);
 textTweet.addEventListener('click', expandTweetBox);
 textTweet.addEventListener('keyup', countChar)
 textTweet.addEventListener('keyup', adjustHeight);
 tweetBtn.addEventListener('click', addTweet);
 
-function checkClickOut(event) {
-  if ((event.target).closest('#new-tweet-box')) {
-    // textTweet.style.backgroundColor = 'yellow';
-    console.log((event.target).closest('#new-tweet-box'));
-  }
-  //   textTweet.className = '';
-    console.log(event);
-    event.stopPropagation();
-}
-
+// Función para expandir tweetBox al hacer click
 function expandTweetBox(event) {
   if (event.target.matches('textarea')) {
     event.target.className = 'expand-box';
@@ -30,6 +21,17 @@ function expandTweetBox(event) {
   }
 }
 
+// Función para colapsar tweetBox al hacer click fuera
+function checkClickOut(event) {
+  var tweetBox = document.getElementById('new-tweet-box');
+  if (event.target !== tweetBox && event.target.parentElement !== tweetBox && charCounter == 0) {
+    textTweet.className = '';
+    tweetBtn.className = 'hidden';
+    totalChar.className = 'hidden';
+  }
+}
+
+// Función para contar caracteres al escribir tweet
 function countChar(event) {
   charCounter = textTweet.value.length;
   totalChar.textContent = 140 - charCounter;
@@ -37,23 +39,46 @@ function countChar(event) {
   if (charCounter > 0 && charCounter <=140) {
     tweetBtn.disabled = false;
   } else tweetBtn.disabled = true;
-}
 
-function adjustHeight(event) {
-  if (textTweet.scrollHeight > 75) {
-    textTweet.style.transitionDuration = null;
-    textTweet.style.height = textTweet.scrollHeight + 'px';
+  // Cambiar el color del contador
+  switch (true) {
+    case charCounter > 120 && charCounter <= 130:
+      totalChar.style.color = '#F7B32B';
+      break;
+    case charCounter > 130 && charCounter <= 140:
+      totalChar.style.color = '#FE5E41';
+      break;
+    case charCounter > 140:
+      totalChar.style.color = '#C81D25';
+      break;
+    default:
+      totalChar.style.color = '';
   }
 }
 
+// Función para reajustar el tamaño de tweetBox
+function adjustHeight(event) {
+  // if (textTweet.scrollHeight) {
+  //   textTweet.style.transitionDuration = null;
+  //   textTweet.style.height = textTweet.scrollHeight + 'px';
+  // }
+
+  // if (charCounter > 0) {
+  //   textTweet.style.height = '47px';
+  //   textTweet.style.height = textTweet.scrollHeight + 'px';
+  // }
+}
+
+// Función para agregar tweet nuevo al newsfeed
 function addTweet(event) {
   var newTweet = document.createElement('div');
   newTweet.textContent = textTweet.value;
 
-  // if (newsfeed.children.length == 0)
+  if (newsfeed.children.length == 0)
     newsfeed.appendChild(newTweet);
-  // else newsfeed.insertBefore(newTweet, newsfeed.firstElementChild)
+  else newsfeed.insertBefore(newTweet, newsfeed.firstElementChild)
   
+  // Resetear textTweet al estado inicial
   textTweet.value = '';
   textTweet.className = '';
   tweetBtn.className = 'hidden';
